bbc3a0cda043dc01a3045dc76235ca0296e791c9
Guilherme Prigol Kramer <guilhermeprigolkramer@gmail.com>
2026-03-18 17:08:06 -0300

    correção no controller do login callback

--- src/auth/auth_controller.py
+++ src/auth/auth_controller.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from src.auth.use_cases.login.login_controller import router as login_controller
+from .use_cases.login_callback.login_callback_controller import router as login_callback_controller
 
 router = APIRouter()
 
 router.include_router(login_controller)
+router.include_router(login_callback_controller)
